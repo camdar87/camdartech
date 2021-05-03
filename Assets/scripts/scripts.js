d4fbceba0d8fe7d038e5b228627a109648f62c33
dargcl1 <dargcl1@D201-62605>
2021-05-03 16:55:45 +1200

    added fix to nav bar

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -1,10 +1,8 @@
 function MobileDrop() {
     let dropdown = document.getElementById("myDIV");
-    let boxeffect = document.getElementById("p");
     dropdown.style.display == "none"
     if (dropdown.style.display === "block") {
     dropdown.style.display = "none";
-    boxeffect.style.backgroundColor= "black";
     } else {
     dropdown.style.display = "block";
     }
