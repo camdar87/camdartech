cba7117980e1cdabf150a60a7d78a1246321628c
dargcl1 <dargcl1@D201-62605>
2021-05-12 14:56:56 +1200

    added mobile section

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -1,21 +1,21 @@
 function MobileDrop() {
     let dropdown = document.getElementById("myDIV");
-    dropdown.style.display = "block";
+    dropdown.style.display = "none";
 }
 function MobileOff() {
   let dropdown = document.getElementById("myDIV");
-    //setTimeout(function(){ dropdown.style.display = "none"; }, 2000)
-    dropdown.style.display = "none"
+    setTimeout(function(){ dropdown.style.display = "none"; }, 2000)
+    //dropdown.style.display = "none"
 }
 //tv
 function ContactDrop() {
   let dropdown = document.getElementById("myDIV-2");
-  dropdown.style.display = "block";
+  dropdown.style.display = "none";
 }
 function ContactTV() {
 let dropdown = document.getElementById("myDIV-2");
-  dropdown.style.display = "none"
-  //setTimeout(function(){ dropdown.style.display = "none"; }, 2000)
+  //dropdown.style.display = "none"
+  setTimeout(function(){ dropdown.style.display = "none"; }, 2000)
 }
 
 
