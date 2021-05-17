c41f1820d94bcf71bc61aa8104af6c51585de0d6
dargcl1 <dargcl1@D201-62605>
2021-05-17 16:41:27 +1200

    added comment in javascript

--- Assets/scripts/mobile.js
+++ Assets/scripts/mobile.js
@@ -3,7 +3,7 @@ function MobileImg() {
     Start.style.textDecoration = "underline";  
     Start.style.color = "black"
     }
-    //
+    //uses for mobile nav menu and pictures
     function MobileImg2() {
     let Start = document.getElementById("M-item-2");
     Start.style.textDecoration = "underline";
