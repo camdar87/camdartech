606e10b8766a2dd113759f2f9032fd2476e3cb7a
dargcl1 <dargcl1@D201-62605>
2021-05-17 16:37:24 +1200

    made changes to mobile nav js

--- Assets/scripts/mobile.js
+++ Assets/scripts/mobile.js
@@ -1,12 +1,10 @@
 function MobileImg() {
-    let item1 = 1;
     let Start = document.getElementById("M-item-1");
     Start.style.textDecoration = "underline";  
+    Start.style.color = "black"
     }
     //
     function MobileImg2() {
-        item1=0
     let Start = document.getElementById("M-item-2");
     Start.style.textDecoration = "underline";
-    let item2 = 1;
     }
