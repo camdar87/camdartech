f577346d7ca5db4d0833fe678df49a169052c15b
dargcl1 <dargcl1@D201-62605>
2021-05-26 15:01:48 +1200

    added new id for showing and hidding divs

--- Assets/scripts/mobile.js
+++ Assets/scripts/mobile.js
@@ -1,9 +1,12 @@
-
 document.querySelectorAll(".mobile-nav-itmes").forEach(element => {
     element.addEventListener("click", e => {
         document.querySelectorAll(".mobile-nav-itmes").forEach(el => {
             el.style.textDecoration = "none";
+            el.style.border = "none";
         });
         e.target.style.textDecoration = "underline";
+        e.target.style.border = "2px dashed #000000";
+        console.log (e.target.id)
+
     })
 })
