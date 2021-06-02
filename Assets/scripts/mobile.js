7e0bae5740ad540973d4bb50d115cec798fd41f0
dargcl1 <dargcl1@D201-62605>
2021-06-02 16:08:28 +1200

    added gitignore with node moudles

--- Assets/scripts/mobile.js
+++ Assets/scripts/mobile.js
@@ -2,10 +2,10 @@ document.querySelectorAll(".mobile-nav-itmes").forEach(element => {
     element.addEventListener("click", e => {
         document.querySelectorAll(".mobile-nav-itmes").forEach(el => {
             el.style.textDecoration = "none";
-            el.style.border = "none";
+            // el.style.border = "none";
         });
         e.target.style.textDecoration = "underline";
-        e.target.style.border = "2px dashed #000000";
+        // e.target.style.border = "2px dashed #000000";
         console.log (e.target.id)
     })
 })
