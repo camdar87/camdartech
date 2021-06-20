5cfe2ec6f56862a397ef48993b480f8766c22357
camdar87 <dargcl1@student.op.ac.nz>
2021-06-20 22:46:42 +1200

    added some comments in the main drop down

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -3,12 +3,11 @@ console.log("imgs are fetched from here: https://camdarwebdata.herokuapp.com/") 
 document.querySelectorAll(".nav-main-itmes").forEach(element => {
   element.addEventListener("mouseover", e => {
       document.querySelectorAll(".nav-main-itmes").forEach(el => {
-        //e.target.style.display = "none";
+        e.target.style.display = "none";
 
 
       });
-      //e.target.style.textDecoration = "underline";
-      // e.target.style.border = "2px dashed #000000";
+
      document.querySelectorAll(".item_box").forEach(m => {
          m.style.display = "none";
      })
