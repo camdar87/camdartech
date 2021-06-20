e4b4d072692d499db3f9d643f6d28c32dc07f477
camdar87 <dargcl1@student.op.ac.nz>
2021-06-20 23:08:14 +1200

    added some js for nav but there is a bug

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -1,18 +1,21 @@
 console.log("imgs are fetched from here: https://camdarwebdata.herokuapp.com/") // console host img site 
 //used for drop down
+
+
+
 document.querySelectorAll(".nav-main-itmes").forEach(element => {
   element.addEventListener("mouseover", e => {
       document.querySelectorAll(".nav-main-itmes").forEach(el => {
-        e.target.style.display = "none";
-
-
+          el.style.textDecoration = "block";
+          // el.style.border = "none";
       });
-
-     document.querySelectorAll(".item_box").forEach(m => {
-         m.style.display = "none";
+     // e.target.style.textDecoration = "underline";
+      // e.target.style.border = "2px dashed #000000";
+     document.querySelectorAll(".item-drop-down").forEach(m => {
+       m.style.display = "block";
+       console.log(e.target.id)
      })
      // document.querySelector("."+e.target.id).style.display = "block";
-
       
   })
 })
