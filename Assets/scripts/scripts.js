d073db041963c49259866f6997b5a9edeeca144a
dargcl1 <dargcl1@D201-62605>
2021-06-22 16:59:12 +1200

    reworking dropdown menu agian changint it to a css mega menu as i have a java script issue i don have time to fix the mega menu will work the same as the java script one

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -3,40 +3,21 @@ console.log("imgs are fetched from here: https://camdarwebdata.herokuapp.com/") 
 
 
 
-// document.querySelectorAll(".nav-main-itmes").forEach(element => {
-//   element.addEventListener("mouseover", e => {
-//       document.querySelectorAll(".nav-main-itmes").forEach(el => {
-//           el.style.textDecoration = "block";
-//           // el.style.border = "none";
-//       });
-//      // e.target.style.textDecoration = "underline";
-//       // e.target.style.border = "2px dashed #000000";
-//      document.querySelectorAll(".item-drop-down").forEach(m => {
-//        m.style.display = "block";
-//        console.log(e.target.id)
-//      })
-//      // document.querySelector("."+e.target.id).style.display = "block";
-      
-//   })
-// })
-//mobile img and names
-
-//drop down items
-document.querySelectorAll(".Side-menu").forEach(element => {
+document.querySelectorAll(".nav-main-itmes").forEach(element => {
   element.addEventListener("mouseover", e => {
-      document.querySelectorAll(".Side-menu").forEach(el => {
-         el.style.border = "block";
+      document.querySelectorAll(".nav-main-itmes").forEach(el => {
+          el.style.textDecoration = "block";
+         // el.style.border = "none";
+         console.log(e.target.id);
       });
      // e.target.style.textDecoration = "underline";
       // e.target.style.border = "2px dashed #000000";
-     document.querySelectorAll(".pick-item-nav").forEach(m => {
-       m.style.display = "none";
-       console.log(e.target.id)
-     })
-      document.querySelector("."+e.target.id).style.display = "block";
+      //document.querySelector("."+e.target.id).style.display = "block";
       
   })
 })
+//mobile img and names
+
 
 
 
