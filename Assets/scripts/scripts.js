b80af4a878598691340861f36e0b4c7ec9bad29c
dargcl1 <dargcl1@D201-62605>
2021-06-22 12:52:38 +1200

    added fix to dropdown sub menu

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -3,23 +3,40 @@ console.log("imgs are fetched from here: https://camdarwebdata.herokuapp.com/") 
 
 
 
-document.querySelectorAll(".nav-main-itmes").forEach(element => {
+// document.querySelectorAll(".nav-main-itmes").forEach(element => {
+//   element.addEventListener("mouseover", e => {
+//       document.querySelectorAll(".nav-main-itmes").forEach(el => {
+//           el.style.textDecoration = "block";
+//           // el.style.border = "none";
+//       });
+//      // e.target.style.textDecoration = "underline";
+//       // e.target.style.border = "2px dashed #000000";
+//      document.querySelectorAll(".item-drop-down").forEach(m => {
+//        m.style.display = "block";
+//        console.log(e.target.id)
+//      })
+//      // document.querySelector("."+e.target.id).style.display = "block";
+      
+//   })
+// })
+//mobile img and names
+
+//drop down items
+document.querySelectorAll(".Side-menu").forEach(element => {
   element.addEventListener("mouseover", e => {
-      document.querySelectorAll(".nav-main-itmes").forEach(el => {
-          el.style.textDecoration = "block";
-          // el.style.border = "none";
+      document.querySelectorAll(".Side-menu").forEach(el => {
+         el.style.border = "block";
       });
      // e.target.style.textDecoration = "underline";
       // e.target.style.border = "2px dashed #000000";
-     document.querySelectorAll(".item-drop-down").forEach(m => {
-       m.style.display = "block";
+     document.querySelectorAll(".pick-item-nav").forEach(m => {
+       m.style.display = "none";
        console.log(e.target.id)
      })
-     // document.querySelector("."+e.target.id).style.display = "block";
+      document.querySelector("."+e.target.id).style.display = "block";
       
   })
 })
-//mobile img and names
 
 
 
